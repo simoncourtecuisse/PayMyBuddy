34c504d2a852af8066fe4b1d34f02d3faf9d5815
Simon Courtecuisse <simoncourtecuisse@gmail.com>
2021-11-15 13:55:27 +0100

    start

--- src/main/java/com/openclassrooms/payMyBuddy/config/SecurityConfiguration.java
+++ src/main/java/com/openclassrooms/payMyBuddy/config/SecurityConfiguration.java
@@ -11,6 +11,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-
+        http.addFilter()
     }
 }
